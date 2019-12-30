e8803d039ce8e4d6fc07b0e418bbaba281673f02
megmiyata <megmiyata@hotmail.com>
2019-12-30 00:19:24 -0300

    header finalizado

--- header.php
+++ header.php
@@ -1,16 +1,33 @@
 <!DOCTYPE html>
-<html lang="en">
+<html lang="pt-br">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Document</title>
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
+    <link rel="stylesheet" href="assets/css/header.css">
+    <link rel="stylesheet" href="assets/css/footer.css">
+    <script defer src="https://kit.fontawesome.com/c9300953f9.js" crossorigin="anonymous"></script>
 </head>
 <body>
+    <header>
 
-    <!-- LOGO -->
+        <div>
+            <img src="assets/img/logo.png" alt="Logo">
+        </div>
+    
+    </header>
 
-    <div></div>
+    <nav>
+        <ul class="container">
+            <li><a href="">Home</a></li>
+            <li><a href="">Sobre</a></li>
+            <li><a href="">Serviços</a></li>
+            <li><a href="">Contato</a></li>
+            <li><a href="">Localização</a></li>
+            <li><a href="">Eventos</a></li>
+        </ul>
+    </nav>
 
-
-    <!-- Menu -->
+    
