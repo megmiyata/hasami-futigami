40837ebe4a29d1618b36448dbf8e6ffeb87cff5f
megmiyata <megmiyata@hotmail.com>
2020-01-09 18:21:18 -0300

    servicos iniciado

--- header.php
+++ header.php
@@ -7,6 +7,7 @@
     <title>Document</title>
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
     <link rel="stylesheet" href="assets/css/header.css">
+    <link rel="stylesheet" href="assets/css/index.css">
     <link rel="stylesheet" href="assets/css/footer.css">
     <script defer src="https://kit.fontawesome.com/c9300953f9.js" crossorigin="anonymous"></script>
 </head>
@@ -21,9 +22,9 @@
 
     <nav>
         <ul class="container">
-            <li><a href="">Home</a></li>
-            <li><a href="">Sobre</a></li>
-            <li><a href="">Serviços</a></li>
+            <li><a href="index.php">Home</a></li>
+            <li><a href="sobre.php">Sobre</a></li>
+            <li><a href="servicos.php">Serviços</a></li>
             <li><a href="">Contato</a></li>
             <li><a href="">Localização</a></li>
             <li><a href="">Eventos</a></li>
---
+++ servicos.php
@@ -0,0 +1,78 @@
+<?php 
+
+    include 'header.php';
+    // include 'footer.php';
+    
+?>
+
+    <div class="servicos container mt-3"> 
+        <div class="row">
+            <div class="col-md-6 col-lg-3">
+                <div class="card">
+                    <img src="assets/img/make1.jpg" class="card-img-top" alt="make">
+                    <div class="card-body">
+                        <h5 class="card-title">Card title</h5>
+                        <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                    </div>
+                </div>
+            </div>
+
+            <div class="col-md-6 col-lg-3">
+                <div class="card">
+                    <img src="assets/img/make2.jpg" class="card-img-top" alt="make">
+                    <div class="card-body">
+                        <h5 class="card-title">Card title</h5>
+                        <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                        
+                    </div>
+                </div>
+            </div>
+
+            <div class="col-md-6 col-lg-3">
+                <div class="card">
+                    <img src="assets/img/cabelo-homi.jpg" class="card-img-top" alt="make">
+                    <div class="card-body">
+                        <h5 class="card-title">Card title</h5>
+                        <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                        
+                    </div>
+                </div>
+            </div>
+
+            <div class="col-md-6 col-lg-3">
+                <div class="card">
+                    <img src="assets/img/cabelo-homi.jpg" class="card-img-top" alt="make">
+                    <div class="card-body">
+                        <h5 class="card-title">Card title</h5>
+                        <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                        
+                    </div>
+                </div>
+            </div>
+        </div>
+
+        <div class="servicos container mt-3"> 
+            <div class="row">
+                <div class="col-md-6 col-lg-3">
+                    <div class="card">
+                        <img src="assets/img/cabelo1.jpg" class="card-img-top" alt="cabelo">
+                        <div class="card-body">
+                            <h5 class="card-title">Card title</h5>
+                            <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                            
+                        </div>
+                    </div>
+                </div>
+
+                <div class="col-md-6 col-lg-3">
+                    <div class="card">
+                        <img src="assets/img/noiva1.jpg" class="card-img-top" alt="noiva">
+                        <div class="card-body">
+                            <h5 class="card-title">Card title</h5>
+                            <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                            
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
