387d8a0df614eafd65e9b010b23f342a6ad2650b
megmiyata <megmiyata@hotmail.com>
2020-01-09 17:45:56 -0300

    sobre iniciado

---
+++ sobre.php
@@ -0,0 +1,26 @@
+<?php 
+
+    include 'header.php';
+    // include 'footer.php';
+    
+?>
+
+    <div class="sobre text-center">
+        <div class="container">
+            <div class="row">
+                <div class="mt-3">
+
+                    <h1>Sobre</h1>
+                
+                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Aut dolores quasi corrupti sapiente obcaecati ipsa? A reprehenderit ipsum, nisi id dolorum quas similique explicabo, modi nihil, non repudiandae harum nobis.</p>
+
+                    <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Aliquam earum quis qui ut atque doloribus optio ratione, velit quaerat accusamus hic amet, est error dolor fugiat odit impedit repudiandae laudantium?</p>
+
+                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolorum in, itaque consectetur nesciunt possimus, sapiente incidunt velit, excepturi quisquam expedita optio libero qui assumenda a voluptatibus tenetur dicta quam ratione.</p>
+                    
+                    <img src="assets/img/equipamentos.jpg" class="img-fluid" alt="Equipamentos">  
+
+                </div>
+            </div>
+        </div>
+    </div>
