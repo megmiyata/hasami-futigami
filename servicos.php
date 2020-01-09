32ab5a461bafc6b40d643ecfd11d2af7d5dd2ced
megmiyata <megmiyata@hotmail.com>
2020-01-09 18:25:14 -0300

    servicos modificado

--- servicos.php
+++ servicos.php
@@ -66,6 +66,28 @@
 
                 <div class="col-md-6 col-lg-3">
                     <div class="card">
+                        <img src="assets/img/cabelo1.jpg" class="card-img-top" alt="cabelo">
+                        <div class="card-body">
+                            <h5 class="card-title">Card title</h5>
+                            <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                            
+                        </div>
+                    </div>
+                </div>
+
+                <div class="col-md-6 col-lg-3">
+                    <div class="card">
+                        <img src="assets/img/noiva1.jpg" class="card-img-top" alt="noiva">
+                        <div class="card-body">
+                            <h5 class="card-title">Card title</h5>
+                            <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                            
+                        </div>
+                    </div>
+                </div>
+
+                <div class="col-md-6 col-lg-3">
+                    <div class="card">
                         <img src="assets/img/noiva1.jpg" class="card-img-top" alt="noiva">
                         <div class="card-body">
                             <h5 class="card-title">Card title</h5>
@@ -76,3 +98,4 @@
                 </div>
             </div>
         </div>
+    </div>
