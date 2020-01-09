6eb82a1d9ab21588c797c48b5cc6dddda2ecabe2
megmiyata <megmiyata@hotmail.com>
2020-01-09 17:35:52 -0300

    home iniciada

--- index.php
+++ index.php
@@ -1,6 +1,20 @@
 <?php 
 
     include 'header.php';
-    include 'footer.php';
+    // include 'footer.php';
     
 ?>
+
+    <div class="home text-center">
+        <div class="container">
+            <div class="row">
+                <div class="mt-3">
+
+                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Aut dolores quasi corrupti sapiente obcaecati ipsa? A reprehenderit ipsum, nisi id dolorum quas similique explicabo, modi nihil, non repudiandae harum nobis.</p>
+
+                    <img src="assets/img/cabelo-homi.jpg" class="img-fluid" alt="Cabelo masculino">  
+
+                </div>
+            </div>
+        </div>
+    </div>
